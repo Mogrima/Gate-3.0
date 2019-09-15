2b0ae3452a396276eb3013da64083167b9c05b15
Mogrima <Rebel.RS@yandex.ru>
2019-09-15 23:53:22 +0400

    1. в attention добавлен стиль для блока ошибки в модальных окнах. 2. в modal-login.php добавлены атрибуты required. 3. добавлен блок ошибки и ссылка на отдельную страницу авторизации, в action прописан путь на обработчик формы

--- core/blocks/modal-login.php
+++ core/blocks/modal-login.php
@@ -1,14 +1,15 @@
 <section class="login login__log">
     <h2 class="visually-hidden">Здесь можно представиться</h2>
     <p class="section-header login__title">Здравствуйте</p>
-    <form method="POST" action="#">
+    <form id="login-form" method="POST" action="core/business/auth_modal.php">
+    <p id="errorLogin" class="attention attention--modal"></p>
       <p class="input__wrapper">
           <label for="login-name" class="visually-hidden">Ваше имя</label>
-          <input id="login-name" class="input login__input login__input--page" type="text" name="username" placeholder="Ваше имя..."">
+          <input id="login-name" class="input login__input login__input--page" type="text" name="username" placeholder="Ваше имя..." required>
       </p>
       <p class="input__wrapper">
           <label for="login-pass" class="visually-hidden">Ваш пароль</label>
-          <input id="login-pass" class="input login__input login__input--page" type="password" name="password" placeholder="Ваш ключ...">
+          <input id="login-pass" class="input login__input login__input--page" type="password" name="password" placeholder="Ваш ключ..." required>
       </p>
         <div class="login__info">
         <input id="login-remember" class="checkbox login__info-checkbox" type="checkbox" name="remember"> <label for="login-remember" class="checkbox__name login__checkbox-name"><span class="checkbox__indicator login__checkbox-indicator"></span>Запомните меня</label>
@@ -16,5 +17,6 @@
       </div>
       <button class="button login__button" type="submit" value="enter" name="submit">Представиться</button>
     </form>
+    <a class="link link--profile" href="./login.php">Если форма работает некорректно</a>
        <button class="close login__close-log">Закрыть</button>
 </section>
