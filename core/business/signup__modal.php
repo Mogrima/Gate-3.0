5d0b450cba502d664c51e7317fb1639afa286c4a
Mogrima <Rebel.RS@yandex.ru>
2019-09-27 17:57:45 +0400

    в регистрации вынесено условие проверки правильности повтора условия в отдельное для большей точности

--- core/business/signup__modal.php
+++ core/business/signup__modal.php
@@ -9,46 +9,51 @@
   $password2 = trim(filter_var($_POST['userpass2'], FILTER_SANITIZE_STRING));
   $email = trim(filter_var($_POST['useremail'], FILTER_SANITIZE_EMAIL));
 
-  if(!empty($username) && !empty($password) && !empty($email) && ($password == $password2)) {
-    $stmt = $pdo->prepare("SELECT COUNT(*) AS count FROM `user` WHERE username=?");
-    $stmt->execute(array($username));
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-      $username_count = $row["count"];
-    }
-    if ($username_count > 0) {
-        echo "Это имя уже кем-то занято";
-    }
-    else {
-      $stmt = $pdo->prepare("SELECT COUNT(*) AS count FROM `user` WHERE user_email=?");
-      $stmt->execute(array($email));
+  if(!empty($username) && !empty($password) && !empty($email)) {
+    if($password == $password2) {
+      $stmt = $pdo->prepare("SELECT COUNT(*) AS count FROM `user` WHERE username=?");
+      $stmt->execute(array($username));
       while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        $email_count = $row["count"];
-        if ($email_count > 0) {
-          echo "Этот адрес электронной почты уже используется";
-        }
-        else {
-          $password = md5($password);
-          $sql = "INSERT INTO user(username, user_email, password) VALUES('$username', '$email', '$password')";
-          $query = $pdo->prepare($sql);
-          $query->execute([$username, $email, $password]);
-
-          $sql = 'SELECT `user_id`, `user_email`, `username` FROM `user` WHERE `username` =:username || `user_email` =:username && `password` =:password';
-          $query = $pdo->prepare($sql);
-          $query->execute(['username' => $username, 'password' => $password]);
-
-          $user = $query->fetch(PDO::FETCH_OBJ);
-
-          $user_id = $user->user_id;
-          $username = $user->username;
-          $_SESSION['user_id'] = $user_id;
-          $_SESSION['username'] = $username;
-          $_SESSION['user_email'] = $user->user_email;
-          setcookie('user_id', $user_id, time() + (60 * 60 * 24 * 30));
-          setcookie('username', $username, time() + (60 * 60 * 24 * 30));
-
-          echo "OK";
+        $username_count = $row["count"];
+      }
+      if ($username_count > 0) {
+          echo "Это имя уже кем-то занято";
+      }
+      else {
+        $stmt = $pdo->prepare("SELECT COUNT(*) AS count FROM `user` WHERE user_email=?");
+        $stmt->execute(array($email));
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+          $email_count = $row["count"];
+          if ($email_count > 0) {
+            echo "Этот адрес электронной почты уже используется";
+          }
+          else {
+            $password = md5($password);
+            $sql = "INSERT INTO user(username, user_email, password) VALUES('$username', '$email', '$password')";
+            $query = $pdo->prepare($sql);
+            $query->execute([$username, $email, $password]);
+  
+            $sql = 'SELECT `user_id`, `user_email`, `username` FROM `user` WHERE `username` =:username || `user_email` =:username && `password` =:password';
+            $query = $pdo->prepare($sql);
+            $query->execute(['username' => $username, 'password' => $password]);
+  
+            $user = $query->fetch(PDO::FETCH_OBJ);
+  
+            $user_id = $user->user_id;
+            $username = $user->username;
+            $_SESSION['user_id'] = $user_id;
+            $_SESSION['username'] = $username;
+            $_SESSION['user_email'] = $user->user_email;
+            setcookie('user_id', $user_id, time() + (60 * 60 * 24 * 30));
+            setcookie('username', $username, time() + (60 * 60 * 24 * 30));
+  
+            echo "OK";
+          }
         }
       }
+    }
+    else {
+      echo "Повтор пароля введён не верно";
     }
   }
   else {
