731924c4afa8c53971e933ee358507eff0896ca5
Mogrima <Rebel.RS@yandex.ru>
2019-11-22 21:13:29 +0400

    в adWorks3.php: 1. переделан вывод содержания по главам. 2. создан массив со всеми номерами страниц и названиям глав, к которым этим страницы относится, благодаря этому сделан отдельный вывод названии главы на каждой страницы. 3. удален лишний и закомментированный код

--- core/admin/adWorks3.php
+++ core/admin/adWorks3.php
@@ -36,12 +36,6 @@
       $chapterPages = count($lines) - 1;
       $num = $chapterPages - $n;
 
-      // $point = "<h2>наташа</h2> ... <h2>даша</h2> ... <h2>настя</h2> ...";
-      //     // var_dump($point);
-      //     if(preg_match_all('|<h2>(.+)</h2>|isU', $point, $arr)) {
-      //       echo "Глава" .$arr[0][0];
-      //     }
-
         // получаем массив с страницами
         $page_arr = array();
         $count1 = (ceil($chapterPages/6)) * 6;
@@ -57,11 +51,6 @@
         }
         // print_r($page_arr);
       //////////////////////////////////////////////////////////////
-      // получаем общее количество строк
-      $stmt = $pdo->query("SELECT COUNT(*) FROM test WHERE title = 'Истории тысячи миров 2'");
-      $row = $stmt->fetch();
-      $countChapters=$row[0]; //количество строк
-      //////////////////////////////////////////////////////////////
       echo "<p style='color: grey'>chapterPages = $chapterPages - кол-во параграфов в главе $chapter.
             <br>n = $n - увеличивается при перелистывании страниц
             <br>num = $num - уменьшается при перелистывании страниц, счетчик оставшихся параграфов
@@ -74,71 +63,34 @@
       function counterPage($page_arr, $n) {
         echo $page_arr[$n];
       }
-      function iterat($n, $num, $lines, $contents, $titleChapterCurrent) {
-        global $y;
-        global $titleChapter;
-        $y = $n;
-        $count = 6; // по умолчанию создается внутри ф-ции
-        $count += $n; // постепенно наращивается
-        echo " $y до итерации равен y.<br>" . $contents[$titleChapterCurrent][1] . "<br>";
-        echo " $count до итерации равен count. ";
-      
-        if (($n - $contents[$titleChapterCurrent][1]) == 0) {
-          echo "num меньше 6 " . $contents[$titleChapterCurrent][1];
-          for ($i = $y; $i < $count; $i++) {
-            $y = $i;
-            echo '<p style="color: red">'.$y.'</p>';
-            echo $lines[$i];
-            if(preg_match_all('|<h2 class="section-book__title">(.+)</h2>|isU', $chapter, $arr)) {
-              // echo "Глава" .$arr[0][0];
-              $titleChapter = $arr[1][0];
-            }
-          }
-        }
-        for ($i = $y; $i < $count; $i++) {
-          echo $lines[$i];
-          $y = $i;
-          $chapter = $lines[$i];
-          // var_dump($chapter);
-          echo '<p style="color: red">'.$y.'</p>';
-          if(preg_match_all('|<h2 class="section-book__title">(.+)</h2>|isU', $chapter, $arr)) {
+      $numbering = array();
+      // function counterContent($page_arr, $numbering) {
+        $z = 0;
+        foreach ($page_arr as $page) {
+          if(preg_match_all('|<h2 class="section-book__title">(.+)</h2>|isU', $page, $arr)) {
             // echo "Глава" .$arr[0][0];
             $titleChapter = $arr[1][0];
           }
-          
+          $numbering[$z] = $titleChapter;
+          $z++;
         }
-        echo " После итерации $y равен y. <br>";
-        echo " $count После итерации равен count. ";
-        return $y;
-      }
-      // сохраняем текущее значение $n до функции
-      $current_n = $n;
+        // return $numbering;
+      // }
+      // print_r($numbering);
+      // Сохраняем название главы в переменную
+      $nameContent = $numbering[$n];
       counterPage($page_arr, $n);
-      // iterat($n, $num, $lines, $contents, $titleChapterCurrent);
-
-//       echo $titleChapter;
-//       // $titleChapter = $titleChapterCurrent;
-//       echo $titleChapterCurrent;
-//       if($titleChapter == '') {
-//         echo "Привет";
-//         $titleChapter = $titleChapterCurrent;
-//       }
-//       else {
-//         echo "Не привет";
-//       }
-
-
-      // $n = $y + 1;
-      // $x = $n - 6 * 2;
 //////////////Нумерация страниц/////////////////////////////////////////////////////////////////
       // счетчик страниц
       $pages = $n + 1;
       $pages_next = $pages + 1;
       $pages_prev = $pages - 1;
 /////////////Конец нумерации страниц////////////////////////////////////////////////////////////     
-      echo "n - $n <br>";
       if ($n == 0) {
         echo "<a href='adWorks3.php?n=".++$n."'>Вперед на ". $pages_next ." страницу</a> ";
+      }
+      else if (count($page_arr) - $n == 1) {
+        echo "<a href='adWorks3.php?n=".--$n."&amp;chapter=".$titleChapter."'>Назад на ". $pages_prev ." страницу</a> ";
       }
       else {
         $x = $n + 1;
@@ -150,19 +102,20 @@
 
 <h3>Содержание</h3>
 <?php
-$sql = "SELECT * FROM `test` WHERE title = 'Истории тысячи миров'";
-$query = $pdo->query($sql);
-$contents = $query->fetchAll(PDO::FETCH_OBJ);
+echo "<br> Текущая глава $nameContent";
 $i = 0;
-foreach ($contents as $content) {
-  if ($comment->id == $id) {
-    // echo "текущее произведение страниц $textid = $i";
-    // return $i;
+$contents = array();
+foreach ($page_arr as $page) {
+  if(preg_match_all('|<h2 class="section-book__title">(.+)</h2>|isU', $page, $arr)) {
+    $titleChapter = $arr[1][0];
+    $contents[$i] = $titleChapter;
   }
   $i++;
-$n = 0;
-$count = 0;
-echo "<h3><a href='adWorks2.php?page=".$i."&amp;n=".$n."'>" . $content->chapter . "</a></h3>";
+}
+// print_r($contents);
+foreach ($contents as $content => $post) {
+  echo "<h3><a href='adWorks3.php?n=".$content."'>" . $post . "</a></h3>";
+  $i++;
 }
 ?>
       <a href="administrator.php" class="button">Назад</a>
