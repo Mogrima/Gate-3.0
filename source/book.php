e9a4666a92ef7e6a007418342a4f2a2af878750b
Mogrima <Rebel.RS@yandex.ru>
2019-12-10 23:59:09 +0400

    в book.php: 1. добавлена переменная id книги, получаемая из get - запроса. 2. изменен запрос sql книги, беря книгу не по фиксированному названию, а по получаемому id со страницы каталога книг. 3. в ссылки вперед и назад, в ссылки содержания добавлена переменная id книги для передачи get-запроса. 4. исправлена ссылка на 'к другим книгам'

--- source/book.php
+++ source/book.php
@@ -6,9 +6,10 @@
   require_once(BUS.'/mysql__connect.php')
 ?>
       <?php
+      $book_id = $_GET["id"];
       $n = isset($_GET["n"]) ? (int) $_GET["n"] : 0;
 
-      $sql = "SELECT * FROM `test` WHERE title = 'Истории тысячи миров 3'";
+      $sql = "SELECT * FROM `works_catalog` WHERE id = $book_id";
       $result = $pdo->query($sql);
       $row = $result->fetch(PDO::FETCH_OBJ);
       $id = $row->id;
@@ -80,7 +81,7 @@
 	<input type="checkbox" id="content-nav__toggle" hidden>
 	<header class="book-header">
 		<h1 class="title book-header__title">Истории тысячи миров</h1>
-		<a class="link-book link-book--to-back" href="../works-catalog.html">К другим книгам</a>
+		<a class="link-book link-book--to-back" href="works-catalog.php">К другим книгам</a>
 	</header>
 			<nav class="content-nav">
 				<label for="content-nav__toggle" class="content-nav__toggle" onclick></label>
@@ -97,7 +98,7 @@
             $i++;
           }
           foreach ($contents as $content => $post) {
-            echo "<li class'content-nav__item'><a class='content-nav__link' href='book.php?n=".$content."'>" . $post . "</a></li>";
+            echo "<li class'content-nav__item'><a class='content-nav__link' href='book.php?id=$book_id&amp;n=".$content."'>" . $post . "</a></li>";
             $i++;
           }?>
 		        </ul>
@@ -126,15 +127,15 @@
         <?php
         if ($n == 0) {
         ?>
-        <a class="link-book page-navigation__link" href="book.php?n=<?=++$n_x?>"><span class="visually-hidden">Следующая страница</span>
+        <a class="link-book page-navigation__link" href="book.php?id=<?=$book_id?>&amp;n=<?=++$n_x?>"><span class="visually-hidden">Следующая страница</span>
 				<svg xmlns="http://www.w3.org/2000/svg" width="100" viewBox="0 0 106.5 17.79"><defs><style>.cls-1{fill:#1a172a;}</style></defs><g id="Слой_2" data-name="Слой 2"><g id="Layer_2" data-name="Layer 2"><path class="cls-1" d="M106.1,8.17a.88.88,0,0,0-.5-.17A76.24,76.24,0,0,1,83.65,5.14a42.32,42.32,0,0,1-4.81-1.75,2.14,2.14,0,1,0-3.31.2l0,.05.06,0a.65.65,0,0,0,.17.16h0l.11.06s0,0,0,0A42.37,42.37,0,0,0,87.65,8H19.42a2.5,2.5,0,0,0-4.66,0h-1.3A3.56,3.56,0,0,0,6.57,8H4.83a2.51,2.51,0,1,0,0,1.79H6.57a3.55,3.55,0,0,0,6.89,0h1.3a2.49,2.49,0,0,0,4.66,0H87.65c-8,1.73-11.78,4.1-11.86,4.15a.9.9,0,0,0-.2.19h0l-.06.07a2.15,2.15,0,1,0,3.32.19c3.7-1.63,12.4-4.61,26.76-4.61a1,1,0,0,0,.35-.07.91.91,0,0,0,.55-.82A.89.89,0,0,0,106.1,8.17ZM10,10.68a1.79,1.79,0,1,1,1.75-2.15.91.91,0,0,0-.08.37.86.86,0,0,0,.08.36A1.79,1.79,0,0,1,10,10.68Z"/></g></g></svg>
         </a>
         <?php
       }
-      else if (count($page_arr) - $n <= 1) {
+      else if (count($page_arr) - $n <= 3) {
         $x_2 = $n - 2;
         ?>
-        <a class="link-book page-navigation__link page-navigation__link--prev" href="book.php?n=<?=$x_2?>"><span class="visually-hidden">Предыдущая страница</span>
+        <a class="link-book page-navigation__link page-navigation__link--prev" href="book.php?id=<?=$book_id?>&amp;n=<?=$x_2?>"><span class="visually-hidden">Предыдущая страница</span>
 				<svg xmlns="http://www.w3.org/2000/svg" width="100" viewBox="0 0 106.5 17.79"><defs><style>.cls-1{fill:#1a172a;}</style></defs><g id="Слой_2" data-name="Слой 2"><g id="Layer_2" data-name="Layer 2"><path class="cls-1" d="M106.1,8.17a.88.88,0,0,0-.5-.17A76.24,76.24,0,0,1,83.65,5.14a42.32,42.32,0,0,1-4.81-1.75,2.14,2.14,0,1,0-3.31.2l0,.05.06,0a.65.65,0,0,0,.17.16h0l.11.06s0,0,0,0A42.37,42.37,0,0,0,87.65,8H19.42a2.5,2.5,0,0,0-4.66,0h-1.3A3.56,3.56,0,0,0,6.57,8H4.83a2.51,2.51,0,1,0,0,1.79H6.57a3.55,3.55,0,0,0,6.89,0h1.3a2.49,2.49,0,0,0,4.66,0H87.65c-8,1.73-11.78,4.1-11.86,4.15a.9.9,0,0,0-.2.19h0l-.06.07a2.15,2.15,0,1,0,3.32.19c3.7-1.63,12.4-4.61,26.76-4.61a1,1,0,0,0,.35-.07.91.91,0,0,0,.55-.82A.89.89,0,0,0,106.1,8.17ZM10,10.68a1.79,1.79,0,1,1,1.75-2.15.91.91,0,0,0-.08.37.86.86,0,0,0,.08.36A1.79,1.79,0,0,1,10,10.68Z"/></g></g></svg>
         </a>
         <?php
@@ -143,10 +144,10 @@
         $x = $n + 1;
         $x_2 = $n - 2;
         ?>
-        <a class="link-book page-navigation__link page-navigation__link--prev" href="book.php?n=<?=$x_2?>"><span class="visually-hidden">Предыдущая страница</span>
+        <a class="link-book page-navigation__link page-navigation__link--prev" href="book.php?id=<?=$book_id?>&amp;n=<?=$x_2?>"><span class="visually-hidden">Предыдущая страница</span>
 				<svg xmlns="http://www.w3.org/2000/svg" width="100" viewBox="0 0 106.5 17.79"><defs><style>.cls-1{fill:#1a172a;}</style></defs><g id="Слой_2" data-name="Слой 2"><g id="Layer_2" data-name="Layer 2"><path class="cls-1" d="M106.1,8.17a.88.88,0,0,0-.5-.17A76.24,76.24,0,0,1,83.65,5.14a42.32,42.32,0,0,1-4.81-1.75,2.14,2.14,0,1,0-3.31.2l0,.05.06,0a.65.65,0,0,0,.17.16h0l.11.06s0,0,0,0A42.37,42.37,0,0,0,87.65,8H19.42a2.5,2.5,0,0,0-4.66,0h-1.3A3.56,3.56,0,0,0,6.57,8H4.83a2.51,2.51,0,1,0,0,1.79H6.57a3.55,3.55,0,0,0,6.89,0h1.3a2.49,2.49,0,0,0,4.66,0H87.65c-8,1.73-11.78,4.1-11.86,4.15a.9.9,0,0,0-.2.19h0l-.06.07a2.15,2.15,0,1,0,3.32.19c3.7-1.63,12.4-4.61,26.76-4.61a1,1,0,0,0,.35-.07.91.91,0,0,0,.55-.82A.89.89,0,0,0,106.1,8.17ZM10,10.68a1.79,1.79,0,1,1,1.75-2.15.91.91,0,0,0-.08.37.86.86,0,0,0,.08.36A1.79,1.79,0,0,1,10,10.68Z"/></g></g></svg>
         </a>
-        <a class="link-book page-navigation__link" href="book.php?n=<?=++$n_x?>"><span class="visually-hidden">Следующая страница</span>
+        <a class="link-book page-navigation__link" href="book.php?id=<?=$book_id?>&amp;n=<?=++$n_x?>"><span class="visually-hidden">Следующая страница</span>
 				<svg xmlns="http://www.w3.org/2000/svg" width="100" viewBox="0 0 106.5 17.79"><defs><style>.cls-1{fill:#1a172a;}</style></defs><g id="Слой_2" data-name="Слой 2"><g id="Layer_2" data-name="Layer 2"><path class="cls-1" d="M106.1,8.17a.88.88,0,0,0-.5-.17A76.24,76.24,0,0,1,83.65,5.14a42.32,42.32,0,0,1-4.81-1.75,2.14,2.14,0,1,0-3.31.2l0,.05.06,0a.65.65,0,0,0,.17.16h0l.11.06s0,0,0,0A42.37,42.37,0,0,0,87.65,8H19.42a2.5,2.5,0,0,0-4.66,0h-1.3A3.56,3.56,0,0,0,6.57,8H4.83a2.51,2.51,0,1,0,0,1.79H6.57a3.55,3.55,0,0,0,6.89,0h1.3a2.49,2.49,0,0,0,4.66,0H87.65c-8,1.73-11.78,4.1-11.86,4.15a.9.9,0,0,0-.2.19h0l-.06.07a2.15,2.15,0,1,0,3.32.19c3.7-1.63,12.4-4.61,26.76-4.61a1,1,0,0,0,.35-.07.91.91,0,0,0,.55-.82A.89.89,0,0,0,106.1,8.17ZM10,10.68a1.79,1.79,0,1,1,1.75-2.15.91.91,0,0,0-.08.37.86.86,0,0,0,.08.36A1.79,1.79,0,0,1,10,10.68Z"/></g></g></svg>
         </a>
         <?php
