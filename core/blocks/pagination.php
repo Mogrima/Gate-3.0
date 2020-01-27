a2640abff2a75e5543f2a842392f8571a5f60069
Mogrima <Rebel.RS@yandex.ru>
2020-01-27 13:27:39 +0400

    в pagination.php: в ссылках передача id книги заменена на переменную, чтобы сделать код универсальным для всего сайта, и в других случаях в переменную ничего не будет добавляться

--- core/blocks/pagination.php
+++ core/blocks/pagination.php
@@ -3,10 +3,10 @@ if ($countPage > 1) {
   ?>
 <ul class="pagination">
   <?php if($page > 1) { ?>
-  <li class="pagination__item"><a href="/book.php?id=<?=$book_id?>&amp;page=1"
+  <li class="pagination__item"><a href="/book.php?page=1<?=$link_add?>"
       class="pagination__arrow  pagination__arrow--prev pagination__arrow--start"><span class="visually-hidden">в
         начало</span></a></li>
-  <li class="pagination__item"><a href="/book.php?id=<?=$book_id?>&amp;page=<?=$page-1;?>"
+  <li class="pagination__item"><a href="/book.php?page=<?=$page-1;?><?=$link_add?>"
       class="pagination__arrow pagination__arrow--prev"><span class="visually-hidden">на предыдущую страницу</span></a>
   </li>
   <?php } 
@@ -18,16 +18,16 @@ $f = $page + $x;
 ($f >= $countPage) ? $f = $countPage : $f;
 for($i = $n; $i<= $f; $i++) { 
 ?>
-  <li class="pagination__item"> <a <?=($i == $page) ? "" : "href='/book.php?id=$book_id&amp;page=$i'";?>
+  <li class="pagination__item"> <a <?=($i == $page) ? "" : "href='/book.php?page=$i$link_add'";?>
       <?=($i == $page) ? 'class="pagination__link pagination__link--current"' : 'class="pagination__link"';?>><?=$i;?></a>
   </li>
   <?php }
 
  if($page < $countPage) { ?>
-  <li class="pagination__item"><a href="/book.php?id=<?=$book_id?>&amp;page=<?=$page+1;?>"
+  <li class="pagination__item"><a href="/book.php?page=<?=$page+1;?><?=$link_add?>"
       class="pagination__arrow pagination__arrow--next"><span class="visually-hidden">на следующую страницу</span></a>
   </li>
-  <li class="pagination__item"><a href="/book.php?id=<?=$book_id?>&amp;page=<?=$countPage;?>"
+  <li class="pagination__item"><a href="/book.php?page=<?=$countPage;?><?=$link_add?>"
       class="pagination__arrow pagination__arrow--next pagination__arrow--finish"><span class="visually-hidden">в
         конец</span></a></li>
   <?php } ?>
