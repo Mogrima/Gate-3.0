60eba8ef7634541b7c383e312c44032c77487ca3
Mogrima <Rebel.RS@yandex.ru>
2020-01-27 13:01:47 +0400

    код пагинации вынесен в отдельный файл и подключен в book.php

---
+++ core/blocks/pagination.php
@@ -0,0 +1,36 @@
+<?php
+if ($countPage > 1) {
+  ?>
+<ul class="pagination">
+  <?php if($page > 1) { ?>
+  <li class="pagination__item"><a href="/book.php?id=<?=$book_id?>&amp;page=1"
+      class="pagination__arrow  pagination__arrow--prev pagination__arrow--start"><span class="visually-hidden">в
+        начало</span></a></li>
+  <li class="pagination__item"><a href="/book.php?id=<?=$book_id?>&amp;page=<?=$page-1;?>"
+      class="pagination__arrow pagination__arrow--prev"><span class="visually-hidden">на предыдущую страницу</span></a>
+  </li>
+  <?php } 
+$x = 1;
+$n = $page - $x;
+($n <= 0) ? $n = 1 : $n;
+($page == 1) ? $x = 2 : $x;
+$f = $page + $x;
+($f >= $countPage) ? $f = $countPage : $f;
+for($i = $n; $i<= $f; $i++) { 
+?>
+  <li class="pagination__item"> <a <?=($i == $page) ? "" : "href='/book.php?id=$book_id&amp;page=$i'";?>
+      <?=($i == $page) ? 'class="pagination__link pagination__link--current"' : 'class="pagination__link"';?>><?=$i;?></a>
+  </li>
+  <?php }
+
+ if($page < $countPage) { ?>
+  <li class="pagination__item"><a href="/book.php?id=<?=$book_id?>&amp;page=<?=$page+1;?>"
+      class="pagination__arrow pagination__arrow--next"><span class="visually-hidden">на следующую страницу</span></a>
+  </li>
+  <li class="pagination__item"><a href="/book.php?id=<?=$book_id?>&amp;page=<?=$countPage;?>"
+      class="pagination__arrow pagination__arrow--next pagination__arrow--finish"><span class="visually-hidden">в
+        конец</span></a></li>
+  <?php } ?>
+</ul>
+<?php } 
+?>
--- source/book.php
+++ source/book.php
@@ -129,34 +129,8 @@
           // echo 'количество строк' . $c .'<br>';
           $countPage = ceil($c / $on_page);
           // echo 'количество страниц' . $countPage .'<br>';
-          if ($countPage > 1) {
-            ?>
-           <ul class="pagination">
-        <?php if($page > 1) { ?>
-            <li  class="pagination__item"><a href="/book.php?id=<?=$book_id?>&amp;page=1" class="pagination__arrow  pagination__arrow--prev pagination__arrow--start"><span class="visually-hidden">в начало</span></a></li>
-            <li  class="pagination__item"><a href="/book.php?id=<?=$book_id?>&amp;page=<?=$page-1;?>" class="pagination__arrow pagination__arrow--prev"><span class="visually-hidden">на предыдущую страницу</span></a></li>
-        <?php } ?>
-        
-        <?php
-          $x = 1;
-          $n = $page - $x;
-          ($n <= 0) ? $n = 1 : $n;
-          ($page == 1) ? $x = 2 : $x;
-          $f = $page + $x;
-          ($f >= $countPage) ? $f = $countPage : $f;
-          for($i = $n; $i<= $f; $i++) { 
+          require_once(BLOCKS . 'pagination.php');
           ?>
-            <li class="pagination__item"> <a <?=($i == $page) ? "" : "href='/book.php?id=$book_id&amp;page=$i'";?> <?=($i == $page) ? 'class="pagination__link pagination__link--current"' : 'class="pagination__link"';?>><?=$i;?></a> </li>
-        <?php } ?>
-        
-        <?php if($page < $countPage) { ?>
-            <li  class="pagination__item"><a href="/book.php?id=<?=$book_id?>&amp;page=<?=$page+1;?>" class="pagination__arrow pagination__arrow--next"><span class="visually-hidden">на следующую страницу</span></a></li>
-            <li  class="pagination__item"><a href="/book.php?id=<?=$book_id?>&amp;page=<?=$countPage;?>" class="pagination__arrow pagination__arrow--next pagination__arrow--finish"><span class="visually-hidden">в конец</span></a></li>
-        <?php } ?> 
-    </ul>
-    <?php } ?> 
-    <!-- вывод пагинатора -->
-          
           </div>
         </section>
           <?php require_once(BLOCKS . 'search-block.php'); ?>
