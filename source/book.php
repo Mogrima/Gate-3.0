f3a70351197ab8df73c1905f8bb019c80a29ce05
Mogrima <Rebel.RS@yandex.ru>
2020-01-31 23:53:26 +0400

    стили для вывода сообщений пользователю при отправки комментария перенесены из блока медиа запроса от планшетной версий в общий блок

--- source/book.php
+++ source/book.php
@@ -73,7 +73,7 @@
           <form action="/book.php?id=<?=$_GET['id']?>" class="reviews__form" method="post">
             <p class="input__wrapper input__wrapper--flex">
               <label class="visually-hidden" for="reviews-massage">Здесь вы можете оставить свой отзыв:</label> 
-              <textarea class="input reviews__massage countInput" id="reviews-massage" maxlength="600" name="reviews" placeholder="ваш отзыв"></textarea>
+              <textarea class="input reviews__massage countInput" id="reviews-massage" maxlength="600" name="reviews" placeholder="ваш отзыв" required></textarea>
               <p class="count-letter">Осталось <span class="count-letter_symbol">600</span> знаков</p>
             </p>
             <?php
