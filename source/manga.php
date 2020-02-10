44755fb335ff10bec65273365f1491de831b4feb
Mogrima <Rebel.RS@yandex.ru>
2020-02-10 23:09:27 +0400

    на страницу манги добавлен динамический вывод страниц манги, удален статичный код, на страницу содержания в ссылку прописан номер главы в get запросе

--- source/manga.php
+++ source/manga.php
@@ -9,6 +9,7 @@
     // подключение к базе данных
     require_once(BUS.'/mysql__connect.php');
     $id = $_GET["id"];
+    $contents = $_GET["contents"];
     // вывод информации об альбоме
     // $album_list = "SELECT `works_title`, `works_desc` FROM `album_list` WHERE id = $id";
     // $query = $pdo->query($album_list);
@@ -17,8 +18,8 @@
     // $album_desc = $album->works_desc;
     $website_title = 'Наследники богов двух миров';
     // вывод рисунков
-    // $album_arts = "SELECT * FROM `album_arts` WHERE album_id = $id ORDER BY `id` DESC";
-    // $query = $pdo->query($album_arts);
+    $album_arts = "SELECT * FROM `manga` WHERE manga_title = 'Наследники богов двух миров' AND num_of_Head = $contents ORDER BY `id` ASC";
+    $query = $pdo->query($album_arts);
     require_once(BUS.'/pagevars.php');
     require_once(BLOCKS .'head.php');?>
     <link href="fotorama/fotorama.css" rel="stylesheet">
@@ -49,45 +50,16 @@
           <p class="page-description">Таким образом реализация намеченных плановых заданий позволяет оценить значение систем массового участия. Таким образом реализация намеченных плановых заданий позволяет оценить значение систем массового участия. Таким образом реализация намеченных плановых заданий позволяет оценить значение систем массового участия.</p>
           </div>
           <div class="fotorama" data-width="800"  data-max-width="100%" data-allowfullscreen="native">
-            <img class="fotorama__img" src="../img/manga/1head/0 - обложка.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/1.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/2.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/4.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/5.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/6.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/7.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/8.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/5.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/9.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/10.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/11.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/12.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/13.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/14.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/15.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/16.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/17.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/18.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/19.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/20.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/21.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/22.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/23.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/24.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/25.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/26.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/27.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/28.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/29.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/30.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/31.jpg">
-            <img class="fotorama__img" src="../img/manga/1head/32.jpg">
+            <?php 
+            while($art = $query->fetch(PDO::FETCH_OBJ)) { ?>
+            <img class="fotorama__img" src="./img/manga/<?=$art->manga_src?><?=$art->works_image?>.jpg">
+           <?php } ?>
           </div>
           <section>
         <?php 
          $book_id = 1;
          $get_id = $book_id;
-        $link_comment = '/album.php';
+        $link_comment = '/manga.php';
         $link_comment_get = "?id=$get_id";
         $comments_table = 'comments_art';
         require_once(BLOCKS .'comment.php');
