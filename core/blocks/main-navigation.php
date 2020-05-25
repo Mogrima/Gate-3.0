b483ece9aa7b8f82279bf761026a18f83d9666a3
Mogrima <Rebel.RS@yandex.ru>
2020-05-25 17:38:36 +0500

    пунктам главного меню всталены массивы с индексами в соответствии с порядком пунктов

--- core/blocks/main-navigation.php
+++ core/blocks/main-navigation.php
@@ -1,22 +1,22 @@
 <nav class="page-navigation page-navigation--closed page-navigation--nojs">
     <button class="page-navigation__toggle" type="button"><span class="visually-hidden">Открыть меню</span></button>
       <ul class="page-navigation__list">
-        <li class="page-navigation__item page-navigation__item--active">
+        <li class="page-navigation__item <?php echo $menu_active[0];?>">
           <a class="page-navigation__link" href="index.php">Новости</a>
         </li>
-        <li class="page-navigation__item">
+        <li class="page-navigation__item <?php echo $menu_active[1];?>">
           <a class="page-navigation__link page-navigation__link--green" href="works-catalog.php">Книги</a>
         </li>
-        <li class="page-navigation__item">
+        <li class="page-navigation__item <?php echo $menu_active[2];?>">
           <a class="page-navigation__link page-navigation__link--yellow" href="gallery.php">Галерея</a>
         </li>
-        <li class="page-navigation__item">
+        <li class="page-navigation__item <?php echo $menu_active[3];?>">
           <a class="page-navigation__link page-navigation__link--peach" href="about.php">О нас</a>
         </li>
-        <li class="page-navigation__item">
+        <li class="page-navigation__item <?php echo $menu_active[4];?>">
           <a class="page-navigation__link page-navigation__link--lilac" href="reviews.php">Отзывы</a>
         </li>
-        <li class="page-navigation__item">
+        <li class="page-navigation__item <?php echo $menu_active[5];?>">
           <a class="page-navigation__link page-navigation__link--purple" href="FAQ.php">FAQ</a>
         </li>
       </ul>
