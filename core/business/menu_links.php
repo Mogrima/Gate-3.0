dc516851df72ab8d4b46bb6c260a0cd9778c9315
Mogrima <Rebel.RS@yandex.ru>
2020-06-03 20:04:33 +0500

    изменен скрипт выявления текущего адреса страницы и установки в связи с этим активного пункта главного меню

--- core/business/menu_links.php
+++ core/business/menu_links.php
@@ -1,6 +1,13 @@
 <?php 
 
 ////////////////////////// Динамическое добавление активного класса к текущему пункту меню + для страниц подменю
+
+$adress_current = $_SERVER["REQUEST_URI"];
+// echo "<p style='color: pink;'>Адрес текущей страницы " . $adress_current . "</p>";
+preg_match_all('|/(.+).php|isU', $adress_current, $arr);
+$url_active = $arr[0][0];
+// echo "<p style='color: pink;'>Адрес текущей страницы " . $url_active . "</p>";
+
 $menu_active = array("Новости"=> "", 
                       "Книги"=>"", 
                       "Галерея" => "", 
@@ -8,17 +15,37 @@ $menu_active = array("Новости"=> "",
                       "Отзывы" => "",
                       "FAQ" => "");
 
-$menu = array("Новости"=>"//index.php", 
-                "Книги"=>"//works-catalog.php", 
-                "Галерея" => "//gallery.php", 
-                "О нас" => "//about.php",
-                "Отзывы" => "//reviews.php",
-                "FAQ" => "//FAQ.php");
+$menu = array("Новости"=>"/index.php", 
+                "Книги"=>"/works-catalog.php", 
+                "Галерея" => "/gallery.php", 
+                "О нас" => "/about.php",
+                "Отзывы" => "/reviews.php",
+                "FAQ" => "/FAQ.php");
+
+if ($url_active == '/news.php') {
+  $url_active = "/index.php";
+}
+
+if ($url_active == '/album.php') {
+  $url_active = "/gallery.php";
+}
+
+if ($url_active == '/book.php') {
+  $url_active = "/works-catalog.php";
+}
+
+if ($url_active == '/inner-album.php') {
+  $url_active = "/gallery.php";
+}
+
+if (($url_active == '/login.php') || ($url_active == '/registration.php')) {
+  $url_active = "/index.php";
+}
 
 $i = 0;
 $num_menu;
 foreach ($menu as $item) {
-  if($_SERVER["REQUEST_URI"] == $item) {
+  if($url_active == $item) {
     $num_menu = $i;
   }
   $i++;
