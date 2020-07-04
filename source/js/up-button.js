4bbf5134611fbceaca3561de6692b347462f5445
Mogrima <Rebel.RS@yandex.ru>
2020-07-04 12:32:33 +0500

    добавлена кнопка вверх, плавная прокрутка и стилизация для мобильных

---
+++ source/js/up-button.js
@@ -0,0 +1,29 @@
+let wrapper = document.querySelector('.substrate');
+let upButton = document.createElement('button');
+
+upButton.classList.add('up-button');
+if(window.matchMedia('(min-width: 767px)').matches) {
+  upButton.textContent ='Вверх';
+}
+wrapper.appendChild(upButton);
+
+window.onscroll = function () {
+  if (window.pageYOffset > 600) {
+    upButton.classList.add('shown');
+  } else {
+    upButton.classList.remove('shown');
+    }
+
+};
+
+// jquery
+$(document).ready(function() {
+  $(function() {
+    $(".up-button").click(function() {
+      $("body,html").animate({
+        scrollTop: 0
+      }, 800);
+      return false;
+    });
+  });
+});
