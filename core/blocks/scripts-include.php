db4b3ec276b36db21e488b33ca713de115701c12
Mogrima <Rebel.RS@yandex.ru>
2020-08-15 15:32:06 +0500

    для основных скриптов добавлен атрибут defer

--- core/blocks/scripts-include.php
+++ core/blocks/scripts-include.php
@@ -1,4 +1,4 @@
 <script src="js/jquery.min.js"></script>
-<script src="js/scripts.min.js"></script>
+<script defer src="js/scripts.min.js"></script>
 <?php require_once(BLOCKS .'auth-ajax.php'); ?>
 <?php require_once(BLOCKS .'signup-ajax.php'); ?>
