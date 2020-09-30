4a69bb33174558d54ad6d8fb6bb85e519a8b9b2f
Mogrima <Rebel.RS@yandex.ru>
2020-09-30 22:17:28 +0500

    сделана мобильная версия читалки

--- source/reader.php
+++ source/reader.php
@@ -68,6 +68,8 @@
 <html lang="ru">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta name="format-detection" content="telephone=no">
   <title><?=$title?></title>
   <link href="img/favicon.png" rel="apple-touch-icon" sizes="180x180">
   <link href="img/favicon.png" rel="icon" sizes="32x32" type="image/png">
