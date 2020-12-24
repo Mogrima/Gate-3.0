4158fbc2f9a4b00e914a24d3fa6ad44f3df86edd
Mogrima <rebel.rs@yandex.ru>
2020-12-24 23:15:50 +0400

    добавлен удаленный файл auth-ajax.php для асинхронной авторизации и изменен путь до него

---
+++ core/business/auth-ajax.php
@@ -0,0 +1,29 @@
+<script>
+  $('#login-form').submit(function (event) {
+    event.preventDefault();
+    var username = $('#login-name').val();
+    var password = $('#login-pass').val();
+
+    $.ajax({
+      url: "./core/business/auth_modal.php",
+      type: 'POST',
+      cache: false,
+      data: {
+        'username': username,
+        'password': password
+      },
+      dataType: 'html',
+      success: function (data) {
+        if (data == 'OK') {
+          $('#errorLogin').hide();
+          $('.login__button').text('Загрузка..');
+          // document.location.reload(true); // перезагрузка страницы
+          window.location.replace("./index.php");
+        } else {
+          $('#errorLogin').show();
+          $('#errorLogin').text(data);
+        }
+      }
+    });
+  });
+</script>
