6755b015e298400e96fd68f6055cd1eae989048c
Mogrima <rebel.rs@yandex.ru>
2021-01-02 23:01:16 +0500

    для кастомных шрифтов добавлен фонт-дисплей. Добавлен прелоад шрифтов на страницах

--- core/blocks/head.php
+++ core/blocks/head.php
@@ -3,6 +3,14 @@
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <meta name="format-detection" content="telephone=no">
 <title><?=$website_title?></title>
+<link rel="preload" href="fonts/Cormorant Infant/CormorantInfant-Regular.woff" as="font" crossorigin="anonymous">
+<link rel="preload" href="fonts/Cormorant Infant/CormorantInfant-Regular.woff2" as="font" crossorigin="anonymous">
+<link rel="preload" href="fonts/Cormorant Infant/CormorantInfant-Bold.woff" as="font" crossorigin="anonymous">
+<link rel="preload" href="fonts/Cormorant Infant/CormorantInfant-Bold.woff2" as="font" crossorigin="anonymous">
+<link rel="preload" href="fonts/Vollkorn-Regular.woff" as="font" crossorigin="anonymous">
+<link rel="preload" href="fonts/Vollkorn-Regular.woff2" as="font" crossorigin="anonymous">
+<link rel="preload" href="fonts/Vollkorn-Bold.woff" as="font" crossorigin="anonymous">
+<link rel="preload" href="fonts/Vollkorn-Bold.woff2" as="font" crossorigin="anonymous">
 <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-32x32.png">
