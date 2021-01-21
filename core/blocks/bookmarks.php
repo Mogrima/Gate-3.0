81bd9984f2b3eb6bd188dad6a7e9f072ae5daec7
Mogrima <rebel.rs@yandex.ru>
2021-01-21 22:51:49 +0500

    кнопка закладки на страницах читалки теперь при добавление стр в закладки не исчезает, а остается золотой

--- core/blocks/bookmarks.php
+++ core/blocks/bookmarks.php
@@ -5,10 +5,16 @@
 <form class="book-bookmark__form" action="<?php echo $current_url ?>" method="POST">
   <button class="book-bookmark" type="submit" value="submit" name="submit"><span class="visually-hidden">Добавить закладку</span>
     <svg class="book-bookmark__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-       <path d="M106 0v512l150-145.789L406 512V0z" fill="#cccccc" />
+      <path d="M106 0v512l150-145.789L406 512V0z" fill="#cccccc" />
     </svg>
   </button>
 </form>
-<?php } 
+<?php } else if($bookmark == $current_url) { ?>
+  <button class="book-bookmark__form book-bookmark" type="submit" value="submit" name="button" disabled><span class="visually-hidden">Добавить закладку</span>
+    <svg class="book-bookmark__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+      <path d="M106 0v512l150-145.789L406 512V0z" fill="#efb818" />
+    </svg>
+  </button>
+<?php  }
   }
  ?>
