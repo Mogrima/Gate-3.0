c63cebb8a520d2a6ddc089e7da936055b32955be
Mogrima <rebel.rs@yandex.ru>
2021-01-21 22:57:53 +0500

    для кнопки закладок в читалке добавлен тултип

--- core/blocks/bookmarks.php
+++ core/blocks/bookmarks.php
@@ -3,18 +3,24 @@
       if(empty($bookmark) || $bookmark != $current_url) {
   ?>
 <form class="book-bookmark__form" action="<?php echo $current_url ?>" method="POST">
-  <button class="book-bookmark" type="submit" value="submit" name="submit"><span class="visually-hidden">Добавить закладку</span>
-    <svg class="book-bookmark__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-      <path d="M106 0v512l150-145.789L406 512V0z" fill="#cccccc" />
-    </svg>
-  </button>
+  <div class="tooltip">
+    <button class="book-bookmark" type="submit" value="submit" name="submit"><span class="visually-hidden">Добавить закладку</span>
+      <svg class="book-bookmark__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path d="M106 0v512l150-145.789L406 512V0z" fill="#cccccc" />
+      </svg>
+    </button>
+      <span class="tooltip__text">Добавить закладку</span>
+  </div>
 </form>
 <?php } else if($bookmark == $current_url) { ?>
-  <button class="book-bookmark__form book-bookmark" type="submit" value="submit" name="button" disabled><span class="visually-hidden">Добавить закладку</span>
-    <svg class="book-bookmark__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-      <path d="M106 0v512l150-145.789L406 512V0z" fill="#efb818" />
-    </svg>
-  </button>
+  <div class="book-bookmark__form tooltip">
+    <button class="book-bookmark" type="submit" value="submit" name="button" disabled><span class="visually-hidden">Добавить закладку</span>
+      <svg class="book-bookmark__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path d="M106 0v512l150-145.789L406 512V0z" fill="#efb818" />
+      </svg>
+    </button>
+    <span class="tooltip__text">Закладка добавлена</span>
+  </div>
 <?php  }
   }
  ?>
