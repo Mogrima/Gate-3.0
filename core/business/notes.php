b1eb43f98be35e46fc1355179eec3dd2051e4e5e
Mogrima <rebel.rs@yandex.ru>
2021-02-03 23:46:16 +0500

    удалены файлы со старым неиспользуемым кодом из папки bus

--- core/business/notes.php
+++
@@ -1,5 +0,0 @@
-<?php echo '<p> COOKIE ' . $_COOKIE['user_id'] . '</p>';
-      echo '<p> COOKIE ' . $_COOKIE['username'] . '</p>';
-      echo '<p> SESSION ' . $_SESSION['user_id'] . '</p>';
-      echo '<p> SESSION ' . $_SESSION['username'] . '</p>';
-?>
