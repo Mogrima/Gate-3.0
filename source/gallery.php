3fba763e43dfb1c766f171522f05ea283596dd3b
Mogrima <rebel.rs@yandex.ru>
2021-02-09 21:13:50 +0500

    завершен механизм фильтрации на страницы галереи

--- source/gallery.php
+++ source/gallery.php
@@ -29,110 +29,7 @@
             </li>
             <li class="breadcrumb__item breadcrumb__item--current">Галерея</li>
           </ul>
-          <table>
-          <?php require_once(BLOCKS . 'search-block.php'); 
-          
-          function hundler_Filter($arr) {
-            $count_filtres_checked = count($arr);
-          }
-
-          if(isset($_POST['sort'])) {
-            $filters = $_POST['filter'];
-            $count_filtres_checked = count($filters);
-            // get_Filtred_works();
-            $title = 'works_title';
-            $src = 'works_image';
-            $type = 'album_arts';
-            $filter_sql = "SELECT  $title, $src FROM $type WHERE ";
-          for($i = 0; $i < $count_filtres_checked; $i++) {
-            $filters[$i] = $filters[$i] . " = 1 OR ";
-            $filter_sql = $filter_sql . $filters[$i];
-          }
-          // for($i = 0; $i < $count_filtres_checked; $i++) {
-            
-          // }
-
-          $filter_sql = substr($filter_sql, 0, -4);
-          
-          // $filter_sql1 = $filter_sql1 . "\"";
-         // $filter_sql1 = "SELECT works_title, works_image FROM album_arts WHERE ill_of_books = 1 OR b_a_w = 1 OR history = 1";
-
-          $query = $pdo->query($filter_sql);
-          
-          }
-          echo var_dump($filters);
-          $filtred_works = array();
-          echo  $filter_sql;
-
-          function get_Filtred_works() {
-            $sql = "SELECT works_title, works_image FROM album_arts";
-            $query = $pdo->prepare($sql);
-            $query->execute();
-            $row = $query->fetch(PDO::FETCH_OBJ);
-          }
-          ?>
-           </table>
-           <section class="gallery gallery-no-js">
-         <div class="slider__container"> 
-                <!-- <li id="slide_slice"> --> 
-                <ul class="slider__list"> 
-                <?php
-                $album_name = array();
-                $album_src = array();
-                while($row = $query->fetch(PDO::FETCH_OBJ)) {
-
-                  $title_works = $row->$title;
-                  $title_src = $row->$src;
-                  $album_name[] = $row->$title;
-                    $album_src[] = $row->$src;
-        
-                  // echo "
-                  // <tr>
-                  //     <td>$title_works</td>
-                  //     <td>$title_src</td>
-                  //     <td>$row->color Цветные</td>
-                  //     <td>$row->history сюжетные</td>
-                  //     <td>$row->b_a_w черно-белые</td>
-                  //     <td>$row->ill_of_books иллюстрации</td>
-                  // </tr>";
-                  // }
-                  // while($art = $query->fetch(PDO::FETCH_OBJ)) {
-                    
-                   } 
-                  $arts_count = count($album_name);
-                  for($i = 0; $i < $arts_count; $i++) {
-                     ?>
-                    <li class="slider__item">
-                      <h3 class="works__title album-slider__title"><?=$album_name[$i]?></h3>
-                      <img class="slider__img" src="img/<?=$album_src[$i]?>.jpg" width="768px" alt="<?=$album_name[$i]?>">
-                      <div class="count count-no-js"><span>1/7</span></div>
-                    </li> 
-                  <?php }?>
-                </ul> 
-                <!-- </div> --> 
-                 <!-- Подсчет слайдов --> 
-               <div class="count count-js"> 
-                  <span class="count__current">1</span> из 
-                  <span class="count__total">5</span> 
-               </div>
-               
-               </div> 
-               <button class="slider__next album-slider__next"><!-- Следущий --></button> 
-               <button class="slider__prev album-slider__prev"><!-- Предыдущий--></button> 
-               <div class="slider__container-preview"> 
-                <ul class="slider__list-preview"> 
-                 <?php
-                 for($i = 0; $i < $arts_count; $i++) { ?>
-                    <li class="slider__item-preview">
-                      <img class="slider__img-nav" src="img/<?=$album_src[$i]?>-preview.jpg" width="150" alt="<?=$album_name[$i]?>">
-                    </li> 
-                 <?php } ?>
-                </ul> 
-               </div> 
-               <ul class="slider__dots"> 
-               </ul> 
-              
-     </section>
+          <?php require_once(BLOCKS . 'search-block.php'); ?>
           <p class="page-description">Рисование будоражило меня с незапамятных лет, часто открывая моей памяти дорогу к истокам.
              При помощи графического планшета и вдохновенного настроя светлой грусти порой возможно совершить чудо - воссоздать 
              живых и мертвых, пробудить историю, да и просто дать полную волю вымыслу.</p>
@@ -158,6 +55,98 @@
             </fieldset><button class="filter__button filter__button--center" type="submit" value="sort" name="sort">Применить</button>
           </form>
         </section>
+        <?php 
+         $title = 'works_title';
+         $src = 'works_image';
+         $type = 'album_arts';
+
+        function get_Filtred_works($filters, $title, $src, $type) {
+          $count_filtres_checked = count($filters);
+          $filter_sql = "SELECT  $title, $src FROM $type WHERE ";
+          for($i = 0; $i < $count_filtres_checked; $i++) {
+            $filters[$i] = $filters[$i] . " = 1 OR ";
+            $filter_sql = $filter_sql . $filters[$i];
+          }
+          $filter_sql = substr($filter_sql, 0, -4);
+          $filter_sql = $filter_sql . " ORDER BY `id` DESC";
+          return $filter_sql;
+          }
+  
+        if(isset($_POST['sort'])) {
+            $filters = $_POST['filter'];
+            // $count_filtres_checked = count($filters);
+            $filter_sql = get_Filtred_works($filters, $title, $src, $type);
+
+          // $filter_sql = "SELECT  $title, $src FROM $type WHERE ";
+          // for($i = 0; $i < $count_filtres_checked; $i++) {
+          //   $filters[$i] = $filters[$i] . " = 1 OR ";
+          //   $filter_sql = $filter_sql . $filters[$i];
+          // }
+          // for($i = 0; $i < $count_filtres_checked; $i++) {
+            
+          // }
+
+          // $filter_sql = substr($filter_sql, 0, -4);
+          // $filter_sql = $filter_sql . " ORDER BY `id` DESC";
+
+         // $filter_sql1 = "SELECT works_title, works_image FROM album_arts WHERE ill_of_books = 1 OR b_a_w = 1 OR history = 1";
+
+          $query = $pdo->query($filter_sql);
+          
+          //}
+          echo var_dump($filters);
+          echo  $filter_sql;
+
+          ?>
+           <section class="gallery gallery-no-js">
+         <div class="slider__container"> 
+                <ul class="slider__list"> 
+                <?php
+                $album_name = array();
+                $album_src = array();
+                while($row = $query->fetch(PDO::FETCH_OBJ)) {
+
+                  $title_works = $row->$title;
+                  $title_src = $row->$src;
+                  $album_name[] = $row->$title;
+                    $album_src[] = $row->$src;                    
+                   } 
+                  $arts_count = count($album_name);
+                  for($i = 0; $i < $arts_count; $i++) {
+                     ?>
+                    <li class="slider__item">
+                      <h3 class="works__title album-slider__title"><?=$album_name[$i]?></h3>
+                      <img class="slider__img" src="img/<?=$album_src[$i]?>.jpg" width="768px" alt="<?=$album_name[$i]?>">
+                      <div class="count count-no-js"><span>1/7</span></div>
+                    </li> 
+                  <?php }?>
+                </ul> 
+               <div class="count count-js"> 
+                  <span class="count__current">1</span> из 
+                  <span class="count__total">5</span> 
+               </div>
+               
+               </div> 
+               <button class="slider__next album-slider__next"><!-- Следущий --></button> 
+               <button class="slider__prev album-slider__prev"><!-- Предыдущий--></button> 
+               <div class="slider__container-preview"> 
+                <ul class="slider__list-preview"> 
+                 <?php
+                 for($i = 0; $i < $arts_count; $i++) { ?>
+                    <li class="slider__item-preview">
+                      <img class="slider__img-nav" src="img/<?=$album_src[$i]?>-preview.jpg" width="150" alt="<?=$album_name[$i]?>">
+                    </li> 
+                 <?php } ?>
+                </ul> 
+               </div> 
+               <ul class="slider__dots"> 
+               </ul> 
+             <a class="filter__button filter__button--center" href="./gallery.php">Сбросить фильтр</a>
+     </section>
+     <?php 
+         } // конец Post sort
+      else {
+    ?>
         <?php $works_title = 'Список альбомов и рисунков';
         // выключаем (прячем) заголовок
         $sectionTitleOn = false;
@@ -180,7 +169,8 @@
         $link = '/gallery.php';
         $link_add = "";
         $anchor = '#anchor';
-        require_once(BLOCKS . 'pagination.php'); ?>
+        require_once(BLOCKS . 'pagination.php');
+       } // конец else после if sort ?>
       </div><!-- Подложка -->
     </div>
   </main>
