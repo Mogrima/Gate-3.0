dd21674c0470090cff6674f0a0058065f104f388
Mogrima <rebel.rs@yandex.ru>
2021-05-08 23:58:17 +0500

    исправлена ошибка с выведением активного пункта меню в мобильной версии

--- core/business/menu_links.php
+++ core/business/menu_links.php
@@ -8,31 +8,31 @@ preg_match_all('|/(.+).php|isU', $adress_current, $arr);
 $url_active = $arr[0][0];
 // echo "<p style='color: pink;'>Адрес текущей страницы " . $url_active . "</p>";
 
-$menu = array("//index.php", 
-                "//works-catalog.php", 
-                "//gallery.php", 
-                "//about.php",
-                "//reviews.php",
-                "//FAQ.php");
+$menu = array("/index.php", 
+                "/works-catalog.php", 
+                "/gallery.php", 
+                "/about.php",
+                "/reviews.php",
+                "/FAQ.php");
 
-if ($url_active == '//news.php') {
-  $url_active = "//index.php";
+if ($url_active == '/news.php') {
+  $url_active = "/index.php";
 }
 
-if ($url_active == '//album.php') {
-  $url_active = "//gallery.php";
+if ($url_active == '/album.php') {
+  $url_active = "/gallery.php";
 }
 
-if ($url_active == '//book.php') {
-  $url_active = "//works-catalog.php";
+if ($url_active == '/book.php') {
+  $url_active = "/works-catalog.php";
 }
 
-if ($url_active == '//inner-album.php') {
-  $url_active = "//gallery.php";
+if ($url_active == '/inner-album.php') {
+  $url_active = "/gallery.php";
 }
 
-if (($url_active == '//login.php') || ($url_active == '//registration.php')) {
-  $url_active = "//index.php";
+if (($url_active == '/login.php') || ($url_active == '/registration.php')) {
+  $url_active = "/index.php";
 }
 
 // echo $menu[0];
